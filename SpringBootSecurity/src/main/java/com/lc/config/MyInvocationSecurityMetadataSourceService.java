b72336174214258c2ec18e8ec2ddf49a66e92ee4
lc <liu599178552@sina.com>
2019-04-29 15:55:17 +0800

    更新springSecurity

--- SpringBootSecurity/src/main/java/com/lc/config/MyInvocationSecurityMetadataSourceService.java
+++ SpringBootSecurity/src/main/java/com/lc/config/MyInvocationSecurityMetadataSourceService.java
@@ -1,13 +1,17 @@
 package com.lc.config;
 
 import com.lc.mapper.PermissionMapper;
+import com.lc.model.Permission;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 /**
  * Created by lc
@@ -20,11 +24,38 @@ public class MyInvocationSecurityMetadataSourceService implements FilterInvocati
 
     private HashMap<String,Collection<ConfigAttribute>> map = null;
 
+    /**
+     * 加载权限表中所有权限
+     */
     public void loadResourceDefine(){
+        map = new HashMap<>();
+        Collection<ConfigAttribute> array;
+        ConfigAttribute cfg;
+        List<Permission> permissions = permissionMapper.findAll();
+        for(Permission permission : permissions) {
+            array = new ArrayList<>();
+            cfg = new SecurityConfig(permission.getName());
+            //此处只添加了用户的名字，其实还可以添加更多权限的信息，例如请求方法到ConfigAttribute的集合中去。此处添加的信息将会作为MyAccessDecisionManager类的decide的第三个参数。
+            array.add(cfg);
+            //用权限的getUrl() 作为map的key，用ConfigAttribute的集合作为 value，
+            map.put(permission.getUrl(), array);
+        }
 
     }
     @Override
-    public Collection<ConfigAttribute> getAttributes(Object o) throws IllegalArgumentException {
+    public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+        if(map ==null) loadResourceDefine();
+        //object 中包含用户请求的request 信息
+        HttpServletRequest request = ((FilterInvocation) object).getHttpRequest();
+        AntPathRequestMatcher matcher;
+        String resUrl;
+        for(Iterator<String> iter = map.keySet().iterator(); iter.hasNext(); ) {
+            resUrl = iter.next();
+            matcher = new AntPathRequestMatcher(resUrl);
+            if(matcher.matches(request)) {
+                return map.get(resUrl);
+            }
+        }
         return null;
     }
 
@@ -35,6 +66,6 @@ public class MyInvocationSecurityMetadataSourceService implements FilterInvocati
 
     @Override
     public boolean supports(Class<?> aClass) {
-        return false;
+        return true;
     }
 }
